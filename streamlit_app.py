efbf04b695d86c53398cda78888607152d75ab49
sha-emails-snowflake <139687297+sha-emails-snowflake@users.noreply.github.com>
2023-07-18 19:34:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-print ('hi: ', fruityvice_normalized)
 # output the data in the screen as the table
 streamlit.dataframe(fruityvice_normalized)
